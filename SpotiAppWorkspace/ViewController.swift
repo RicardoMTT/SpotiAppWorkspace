bce40b66b637f4efbe00e909f8102b2876cfec6a
Ricardo Tovar <tricardo003@gmail.com>
2023-12-11 19:54:23 -0500

    Initial Commit

---
+++ SpotiAppWorkspace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotiAppWorkspace
+//
+//  Created by Ricardo Manuel Tovar Taboada on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
